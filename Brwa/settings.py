a3474473ac87f9027af2e3b6cc241409d1315c4e
Aryan <Aryan.golden22@gmail.com>
2025-08-14 19:17:19 +0300

    last test

--- Brwa/settings.py
+++ Brwa/settings.py
@@ -113,7 +113,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
